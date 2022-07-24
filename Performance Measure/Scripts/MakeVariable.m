d35ec3906ea85007c70b9059b7e650f5e9c0e377
Henry Bae <BaeHenryS@outlook.com>
2022-07-24 12:58:43 -0400

    Noise Measurement 1

---
+++ Performance Measure/Scripts/MakeVariable.m
@@ -0,0 +1,39 @@
+ dacv0_100k_1vb_001
+ dacv0_100k_1vb_002
+ dacv0_100k_1vb_003
+
+ dacv0_100k_2vb_001
+ dacv0_100k_2vb_002
+ dacv0_100k_2vb_003
+
+ dacv0_100k_3vb_001
+ dacv0_100k_3vb_002
+ dacv0_100k_3vb_003
+
+ dacv0_100k_4vb_001
+ dacv0_100k_4vb_002
+ dacv0_100k_4vb_003
+
+ dacv0_100k_5vb_001
+ dacv0_100k_5vb_002
+ dacv0_100k_5vb_003
+
+ dacv0_100k_6vb_001
+ dacv0_100k_6vb_002
+ dacv0_100k_6vb_003
+
+ dacv0_100k_7vb_001
+ dacv0_100k_7vb_002
+ dacv0_100k_7vb_003
+
+ dacv0_100k_8vb_001
+ dacv0_100k_8vb_002
+ dacv0_100k_8vb_003
+
+ dacv0_100k_9vb_001
+ dacv0_100k_9vb_002
+ dacv0_100k_9vb_003
+
+ dacv0_100k_10vb_001
+ dacv0_100k_10vb_002
+ dacv0_100k_10vb_003
