140efc3b2986f6331d96b6fac627f199260ac717
Henry Bae <BaeHenryS@outlook.com>
2022-07-24 13:14:22 -0400

    Update NoiseMeasurement 2

--- Performance Measure/Scripts/IterNoiseMeasurement.m
+++ Performance Measure/Scripts/IterNoiseMeasurement.m
@@ -1,43 +1,12 @@
 for i = -10:10
+    j = i + 11;
+    smset("NUDAC", i)
+    pause(1)
+    fprintf(smdata.inst(18).data.inst, 'ARNG');
+    pause(5)
+    fprintf(smdata.inst(18).data.inst, 'AUTS -1');
+    pause(1)
+    fprintf(smdata.inst(18).data.inst, 'STRT');
+    pause(10)
+    dacv0_100k_v(:,j) = NoiseMeasurement(4);
 
-
-
-
-smset("NUDAC", 1)
-
-pause(1)
-fprintf(smdata.inst(18).data.inst, 'ARNG');
-pause(5)
-fprintf(smdata.inst(18).data.inst, 'AUTS -1');
-pause(1)
-
-fprintf(smdata.inst(18).data.inst, 'STRT');
-
-pause(10)
-
-dacv0_100k_1vb_001 = NoiseMeasurement(4);
-
-
-pause(1)
-fprintf(smdata.inst(18).data.inst, 'ARNG');
-pause(5)
-fprintf(smdata.inst(18).data.inst, 'AUTS -1');
-pause(1)
-
-fprintf(smdata.inst(18).data.inst, 'STRT');
-pause(10)
-
-dacv0_100k_1vb_002 = NoiseMeasurement(4);
-
-
-pause(1)
-fprintf(smdata.inst(18).data.inst, 'ARNG');
-pause(5)
-fprintf(smdata.inst(18).data.inst, 'AUTS -1');
-pause(1)
-
-fprintf(smdata.inst(18).data.inst, 'STRT');
-pause(10)
-
-
-dacv0_100k_1vb_003 = NoiseMeasurement(4);
