b093a7e841db226c1f88c4e088c07c1ccbb640b8
Henry Bae <BaeHenryS@outlook.com>
2022-07-25 22:05:34 -0400

    Noise 2

---
+++ Performance Measure/Scripts/DACNoisePlots_0724.m
@@ -0,0 +1,54 @@
+
+%{
+dacv0_195_vb_001_mod = dacv0_195_vb_001;
+
+
+dacv0_195_vb_001_mod(1:10,:) = [];
+
+x_195_mod = x_195;
+
+x_195_mod(:,1:10) = [];
+
+x_195_mod = reshape(x_195_mod, [],1);
+
+
+myfit = fittype('a + b/x',...
+    'dependent',{'y'},'independent',{'x'},...
+    'coefficients',{'a','b'});
+
+f = fit(x_195_mod, dacv0_195_vb_001_mod(:,10), myfit);
+plot(x_195_mod, dacv0_195_vb_001_mod(:,10))
+hold on
+semilogy(x_195_mod, f(x_195_mod))
+%}
+
+%{
+for i = 1:21
+    f = fit(x_195_mod, dacv0_195_vb_001_mod(:,i), myfit);
+    %plot(x_195_mod, dacv0_195_vb_001_mod(:,1))
+    %hold on
+    semilogy(x_195_mod, f(x_195_mod))
+    hold on
+end
+
+%}
+
+dacv0_100k_vb_001_mod = dacv0_100k_vb_001;
+
+
+dacv0_100k_vb_001_mod(1:10,:) = [];
+
+x_100k_mod = x_100k;
+
+x_100k_mod(:,1:10) = [];
+
+x_100k_mod = reshape(x_100k_mod, [],1);
+
+
+
+for i = 1:21
+   plot(x_100k_mod, dacv0_100k_vb_001_mod(:,i), 'DisplayName',string(i))
+   hold on
+end
+
+%colormap(prism(21))
--- Performance Measure/Scripts/IterNoiseMeasurement.m
+++ Performance Measure/Scripts/IterNoiseMeasurement.m
@@ -2,11 +2,16 @@ for i = -10:10
     j = i + 11;
     smset("NUDAC", i)
     pause(1)
-    fprintf(smdata.inst(18).data.inst, 'ARNG');
-    pause(5)
+    fprintf(smdata.inst(18).data.inst, 'ARNG 0');
+    pause(2)
+    fprintf(smdata.inst(18).data.inst, 'ARNG 1');
+    pause(7)
     fprintf(smdata.inst(18).data.inst, 'AUTS -1');
     pause(1)
+    fprintf(smdata.inst(18).data.inst, 'ARNG');
+    pause(1)
     fprintf(smdata.inst(18).data.inst, 'STRT');
-    pause(10)
-    dacv0_100k_v(:,j) = NoiseMeasurement(4);
+    pause(25)
+    dacv0_195_vb_001(:,j) = NoiseMeasurement(4);
+end
 
