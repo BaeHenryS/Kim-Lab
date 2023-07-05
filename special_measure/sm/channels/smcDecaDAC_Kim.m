b79030b88f233a5512768de9e4d6f1457e764b3c
Henry Bae <69275685+BaeHenryS@users.noreply.github.com>
2023-07-05 01:41:28 -0700

    Update smcDecaDAC_Kim.m

--- special_measure/sm/channels/smcDecaDAC_Kim.m
+++ special_measure/sm/channels/smcDecaDAC_Kim.m
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function val = smcDecaDAC4(ic, val, rate)
 % With ramp support and new trigger scheme. Odd channels are ramped.
 % Improved error treatment compared to smcdecaDAC3.m
@@ -114,3 +115,40 @@ while i < 10
         end
     end
 end
+=======
+function val = smcDecaDAC_Kim(ic, val, rate)
+% 1-12 = channel 0 - 11
+
+global smdata;
+
+
+% Implementing the Voltage Send:
+% Instrument: smdata.inst(ic(1))
+% Channel 
+switch ic(3)
+    case 1
+        fprintf("Hi")
+    case 0
+       
+        range = smdata.inst(ic(1)).data.rng(floor((ic(2)-1)/2)+1);
+        fprintf(string(val))
+        fprintf(string(range))
+        if val > range || val < -range
+            voltageBin = 0;
+        else
+            voltageBin = floor((val + range)/(2 * range) * 65535);
+            if voltageBin < 0
+                voltageBin = 0;
+            elseif voltageBin > 65535
+                voltageBin = 65535;
+            end
+            
+        end
+        fprintf(string(voltageBin))
+        fprintf('B %1d;C %1d;M 3;D %5d;', floor((ic(2)-1)/2), mod(ic(2), 2), voltageBin)
+        
+        writeline(smdata.inst(ic(1)).data.inst, sprintf('B %1d;C %1d;M 3;D %5d;', floor((ic(2)-1)/2), mod(ic(2), 2), voltageBin))
+        fprintf("Hello")
+end
+        
+>>>>>>> 69f74633621a0072ed3a03368dbde7bd0bc9ad0f
